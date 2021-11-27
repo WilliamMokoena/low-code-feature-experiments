8bc1d899b1b1b90339919dcd52b505c80d228021
WilliamMokoena <swmokoena412@gmail.com>
2021-11-27 14:50:04 +0000

    Completed sandbox

--- components/utils/Sandbox.tsx
+++ components/utils/Sandbox.tsx
@@ -1,11 +1,25 @@
+import { PureComponent } from "react"
+import { createPortal } from "react-dom"
 
+export default class Sandbox extends PureComponent {
+    private container
+    private iframe
 
-export default function Sandbox() {
-    return (
-        <section>
-            <p>IFRAME</p>
-            <iframe src="/env/pure">
-            </iframe>
-        </section>
-    )
+    constructor(props) {
+        super(props);
+        if (process.browser)
+            this.container = document.createElement("div");
+    }
+
+    render() {
+        return (
+            this.container ? <iframe title="sandbox" ref={el => (this.iframe = el)}>
+                {createPortal(this.props.children, this.container)}
+            </iframe> : ""
+        );
+    }
+
+    componentDidMount() {
+        this.iframe.contentDocument.body.appendChild(this.container);
+    }
 }
